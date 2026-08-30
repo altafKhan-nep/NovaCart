34e14176ae0e5dcfa15867b26fb4288830a95f66
altafkhan-nep <altafkhan.nep@gmail.com>
2026-08-30 20:37:10 +0545

    Fix CORS: add FRONTEND_URL env var support for production

--- server/server.js
+++ server/server.js
@@ -67,7 +67,8 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
-];
+  process.env.FRONTEND_URL,
+].filter(Boolean);
 app.use(
   cors({
     origin(origin, callback) {
